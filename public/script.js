1fc2d1fae5ba857a8f234e6026cb8089699504b0
“Your <74192896+Gyaanendra@users.noreply.github.com>
2023-02-22 18:24:28 +0530

    stage7

--- public/script.js
+++ public/script.js
@@ -18,7 +18,26 @@ navigator.mediaDevices
   .then((stream) => {
     my_stream = stream;
     addVideoStream(my_video, stream);
+
+    socket.on("user connected", (userId) => {
+      connectTOnewUser(userId, stream);
+    });
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
   });
+
+function connectTOnewUser(userId, stream) {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+}
 
 function addVideoStream(video, stream) {
   video.srcObject = stream;
@@ -51,6 +70,39 @@ $(function () {
     if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
       socket.emit("message", $("#chat_message").val());
       $("#chat_message").val("");
+    }
+  });
+
+  $("#mute_button").click(function () {
+    const enabled = my_stream.getAudioTracks()[0].enabled;
+    if (enabled){
+      my_stream.getAudioTracks()[0].enabled = false;
+      html=`<i class="fa fa-microphone-slash"></i>`
+      $("#mute_button").toggleClass("background_red")
+      $("#mute_button").html(html)
+      
+    }
+    else{
+      my_stream.getAudioTracks()[0].enabled = true;
+      html=`<i class="fa fa-microphone"></i>`
+      $("#mute_button").toggleClass("background_red")
+      $("#mute_button").html(html)
+    }
+  });
+  $("#stop_video").click(function () {
+    const enabled = my_stream.getVideoTracks()[0].enabled;
+    if (enabled){
+      my_stream.getVideoTracks()[0].enabled = false;
+      html=`<i class="fa fa-video-camera"></i>`
+      $("#stop_video").toggleClass("background_red_cam")
+      $("#stop_video").html(html)
+
+    }
+    else{
+      my_stream.getVideoTracks()[0].enabled = true;
+      html=`<i class="fa fa-video-camera"></i>`
+      $("#stop_video").toggleClass("background_red_cam")
+      $("#stop_button").html(html)
     }
   });
 });
